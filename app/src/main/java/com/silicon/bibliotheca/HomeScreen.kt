af2c538bc952af9470ee6e0a12a9f542c9c8d50a
HasinaFatema <fatemahasina405@gmail.com>
2025-02-07 20:16:50 +0530

    loginandsignuppage

---
+++ app/src/main/java/com/silicon/bibliotheca/HomeScreen.kt
@@ -0,0 +1,10 @@
+package com.silicon.bibliotheca
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun HomeScreen(navController: NavHostController) {
+
+}
